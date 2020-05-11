8af641df90417f8da0d19f64d383b5217ff3a30d
Rayoule <raoul.desmarest@protonmail.com>
2020-05-11 18:50:27 +0100

    Initial Commit

---
+++ Main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main() {
+    /*
+    int nx = 200;
+    int ny = 100;
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float r = float(i) / float(nx);
+            float g = float(j) / float(ny);
+            float b = 0.2;
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+    */
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
